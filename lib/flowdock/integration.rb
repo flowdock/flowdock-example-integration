5589d9ec1cb756d2e8d71e8e86afdc450a9494d0
Sampo Verkasalo <sampo@flowdock.com>
2014-10-06 10:33:52 +0300

    Simplify and comment flowdock integration example

--- lib/flowdock/integration.rb
+++ lib/flowdock/integration.rb
@@ -1,4 +1,6 @@
 class FlowdockIntegration < ActiveRecord::Base
   validates :token, presence: true, length: { minimum: 1 }
   validates_uniqueness_of :token
+  validates :flowdock_id, presence: true
+  validates_uniqueness_of :flowdock_id
 end
